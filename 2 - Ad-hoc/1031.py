f0d9e096df804566d6fe962088c342240dce6b05
leow14 <leonardoamorimdearaujo@hotmail.com>
2025-10-27 23:32:22 -0300

    Correção do exercício 1031

--- 2 - Ad-hoc/1031.py
+++ 2 - Ad-hoc/1031.py
@@ -1,24 +1,26 @@
 # LINK : https://judge.beecrowd.com/pt/problems/view/1031
 
-#while True:
-n = int(input())
-
-y = 0
-idx = 0
-array = [x for x in range(1, n+1)]
-
 while True:
-    idx = (idx + y - 1) % len(array)
-    array.pop(idx)
-    print(f"y={y}, idx={idx}, array={array}")
-    if 13 not in array:
-        array = [x for x in range(1, n+1)]
-        y += 1
-        idx = 0
-    if len(array) == 1 and 13 in array:
+    n = int(input())
+
+    if n == 0:
         break
-    
+    y = 1
+    idx = 0
+    array = [x for x in range(2, n+1)]
 
-print(y)
+    while True:
+        idx = (idx + y - 1) % len(array)
+        array.pop(idx)
+        #print(f"y={y}, idx={idx}, array={array}")
+        if 13 not in array:
+            array = [x for x in range(2, n+1)]
+            y += 1
+            idx = 0
+        if len(array) == 1 and 13 in array:
+            break
+        
 
-# Autor: Leonardo Amorim de Araujo || Github: https://github.com/Leow14
+    print(y)
+
+    # Autor: Leonardo Amorim de Araujo || Github: https://github.com/Leow14
