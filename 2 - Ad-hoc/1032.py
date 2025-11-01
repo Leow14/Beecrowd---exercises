9bad59312545c973b17549cfc3fae66c2a716df7
leow14 <leonardoamorimdearaujo@hotmail.com>
2025-10-31 22:42:36 -0300

    1032 corrigido, ainda não 100% otimizado

--- 2 - Ad-hoc/1032.py
+++ 2 - Ad-hoc/1032.py
@@ -7,7 +7,7 @@ def ePrimo(n):
     if n % 2 == 0:
         return False
 
-    for i in range(2, n//2):
+    for i in range(2, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
