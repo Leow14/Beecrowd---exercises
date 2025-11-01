934b7d54a06e9b8fd881df7e428401da9f46dfab
leow14 <leonardoamorimdearaujo@hotmail.com>
2025-11-01 13:57:33 -0300

    1068.py concluído

--- 4 - Estruturas e bibliotecas/1068.py
+++ 4 - Estruturas e bibliotecas/1068.py
@@ -4,9 +4,20 @@ while True:
     except EOFError:
         break
 
-    b = 0
+    count = 0
+
     for char in s:
         if char == '(':
-            b += 1
-        elif char == ')' and b >= 1:
+            count += 1
+
+        if char == ')' and count >= 1:
+            count -= 1
+        elif char == ')' and count <=1:
+            count -= 1
+            break
+    
+    if count == 0:
+        print("correct")
+    else:
+        print("incorrect")
             
